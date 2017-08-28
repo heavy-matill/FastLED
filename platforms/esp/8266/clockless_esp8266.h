badfd2eb9b451dec61c331a7f41a219ef9b2deca
Cory R. King <me@coryking.com>
2017-08-27 20:07:38 -0700

    Added RGBW support...  should bury this behind a compiler flag or make it it's own controller...

--- platforms/esp/8266/clockless_esp8266.h
+++ platforms/esp/8266/clockless_esp8266.h
@@ -16,7 +16,7 @@ __attribute__ ((always_inline)) inline static uint32_t __clock_cycles() {
 
 #define FASTLED_HAS_CLOCKLESS 1
 
-template <int DATA_PIN, int T1, int T2, int T3, EOrder RGB_ORDER = RGB, int XTRA0 = 0, bool FLIP = false, int WAIT_TIME = 50>
+template <int DATA_PIN, int T1, int T2, int T3, EOrder RGB_ORDER = RGB, int XTRA0 = 0, bool FLIP = false, int WAIT_TIME = 5>
 class ClocklessController : public CPixelLEDController<RGB_ORDER> {
 	typedef typename FastPin<DATA_PIN>::port_ptr_t data_ptr_t;
 	typedef typename FastPin<DATA_PIN>::port_t data_t;
@@ -62,7 +62,7 @@ protected:
       while((__clock_cycles() - last_mark) < T1);
       if(b & 0x80000000L) { FastPin<DATA_PIN>::lo(); }
       b <<= 1;
-
+q
       while((__clock_cycles() - last_mark) < (T1+T2));
       FastPin<DATA_PIN>::lo();
 		}
@@ -73,23 +73,42 @@ protected:
 	static uint32_t ICACHE_RAM_ATTR showRGBInternal(PixelController<RGB_ORDER> pixels) {
 		// Setup the pixel controller and load/scale the first byte
 		pixels.preStepFirstByteDithering();
-		register uint32_t b = pixels.loadAndScale0();
-    pixels.preStepFirstByteDithering();
+		register out_4px output;
+
+		//register uint32_t b = pixels.loadAndScale0();
+		register  uint32_t minc;
+		output.c0 = pixels.loadAndScale0();
+		pixels.preStepFirstByteDithering();
+
+
 		os_intr_lock();
-    uint32_t start = __clock_cycles();
+		uint32_t start = __clock_cycles();
 		uint32_t last_mark = start;
 		while(pixels.has(1)) {
+
+			output.c1 = pixels.loadAndScale1();
+			output.c2 = pixels.loadAndScale2();
+
+			minc = min(output.c0, output.c1);
+			minc = min(output.c2, minc);
+
+			writeBits<8+XTRA0>(last_mark, output.c0 - minc);
+
 			// Write first byte, read next byte
-			writeBits<8+XTRA0>(last_mark, b);
-			b = pixels.loadAndScale1();
 
 			// Write second byte, read 3rd byte
-			writeBits<8+XTRA0>(last_mark, b);
-			b = pixels.loadAndScale2();
+			writeBits<8+XTRA0>(last_mark, output.c1 - minc);
 
 			// Write third byte, read 1st byte of next pixel
-			writeBits<8+XTRA0>(last_mark, b);
-      b = pixels.advanceAndLoadAndScale0();
+			writeBits<8+XTRA0>(last_mark, output.c2 - minc);
+
+
+			// write out all 0's for the 4th byte for RGBW strips - this is a
+			// hack and will break your RGB WS2812 strips.
+			writeBits<8+XTRA0>(last_mark, minc);
+
+			output.c0 = pixels.advanceAndLoadAndScale0();
+
 
 			#if (FASTLED_ALLOW_INTERRUPTS == 1)
 			os_intr_unlock();
